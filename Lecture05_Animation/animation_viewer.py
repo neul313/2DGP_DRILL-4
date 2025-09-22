9f8fe6d4e96e2e8ca4d563c9ed0b06d48a760cb5
Jo shinBe <tls3123@tukorea.ac.kr>
2025-09-22 23:25:44 +0900

    jump좌표 location으로 수정,해당 프레임만 보임

--- Lecture05_Animation/animation_viewer.py
+++ Lecture05_Animation/animation_viewer.py
@@ -5,8 +5,11 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('sheet.png')
 
-location =[(80,25,115,225,130),(190,45,115,225,150),(300,70,955-870,225,170),(410,45,115,225,155),(520,25,115,225,130)]
+character2 = load_image('sheet_new.png')
 
+location =[(80,25,115,225,130),(190,45,115,225,150),(300,70,955-870,225,170),(410,45,115,225,155),(520,25,115,225,130)]
+location2 =[(90,25,120,225,130),(205,45,310-200,225,150),(310,70,433-310,225,170),
+            (440,45,554-434,225,155),(555,25,668-552,225,130),(670,25,785-670,225,130),]
 def stay():
     frame = 0
     x = 400
@@ -55,12 +58,13 @@ def jump():
     while True:
         clear_canvas()
         grass.draw(400, 30)
-        sx, sy, sw, sh,y = location[frame % len(location)]
-        character.clip_draw(sx, sy, sw, sh, x, y, 200, 200)
+        sx, sy, sw, sh,y = location2[frame % len(location)]
+        character.clip_draw(sx , sy, sw, sh , x, y, 200, 200)
+        #character2.clip_draw(sx,sy, sw, sh , x, y, sw, sh)
 
         update_canvas()
         frame = (frame + 1) % 6
-        delay(0.1)
+        delay(0.2)
 
 
 def ouch():
