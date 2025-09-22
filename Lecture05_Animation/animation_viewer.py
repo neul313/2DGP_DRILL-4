1958ac0ca072e739ca827db4a7d102d054096de6
Jo shinBe <tls3123@tukorea.ac.kr>
2025-09-22 23:51:00 +0900

    location[]설명 추가, 코드 공백 정리

--- Lecture05_Animation/animation_viewer.py
+++ Lecture05_Animation/animation_viewer.py
@@ -6,6 +6,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('sheet.png')
 
+#jump 함수 sheet 프레임 크기 직접 설정 //좌표 직접 계산
 location=[(90,25,120,225,130),(205,45,110,225,150),(310,70,123,225,170),
             (440,45,120,225,155),(555,25,116,225,130),(670,25,115,225,130),]
 
@@ -14,7 +15,6 @@ def stay(frame=0):
     clear_canvas()
     grass.draw(400, 30)
     character.clip_draw(frame * 120 + 25, 717, 120, 238, x, 130, 200, 200)
-
     update_canvas()
     delay(0.1)
 
@@ -22,23 +22,17 @@ def run(frame=0):
     x = 400
     clear_canvas()
     grass.draw(400, 30)
-
     character.clip_draw(frame * 120 + 10 + 730, 495, 116, 238, x, 130, 200, 200)
-
     update_canvas()
     delay(0.1)
-
 
 def walk(frame=0):
     x = 400
     clear_canvas()
     grass.draw(400, 30)
-
     character.clip_draw(frame * 120 + 25, 495, 120, 238, x, 130, 200, 200)
-
     update_canvas()
     delay(0.1)
-
 
 def jump(frame=0):
     x = 400
@@ -48,7 +42,6 @@ def jump(frame=0):
     character.clip_draw(sx, sy, sw, sh, x, y, 200, 200)
     update_canvas()
     delay(0.1)
-
 
 #코파일럿 도움
 def repeat_animation(func, set_count=5, frame_count=6):
